5ab8e4179f7a18e5f5cff5633d039e89924bc747
Bruno Cesar <bruunoocesar@gmail.com>
2021-11-21 17:23:29 -0300

    Adiciona novos parametros na query

--- recent_search.py
+++ recent_search.py
@@ -2,13 +2,14 @@ import requests
 import os
 import json
 from env import chaves
+from datetime import datetime, timedelta
 
 def auth():
     return chaves().get("bearer_token")
 
 
 def create_url():
-    query = "from:twitterdev -is:retweet"
+    query = "AluraOnline"
     # Tweet fields are adjustable.
     # Options include:
     # attachments, author_id, context_annotations,
@@ -16,9 +17,13 @@ def create_url():
     # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
     # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
     # source, text, and withheld
-    tweet_fields = "tweet.fields=author_id"
-    url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}".format(
-        query, tweet_fields
+    tweet_fields = "tweet.fields=author_id,conversation_id,created_at,id,in_reply_to_user_id,public_metrics,text"
+    user_fields = "expansions=author_id&user.fields=id,name,username,created_at"
+    start_time = (datetime.today() - timedelta(days=6)).strftime("%Y-%m-%dT00:00:00Z")
+    end_time = datetime.today().strftime("%Y-%m-%dT00:00:00Z")
+    filters = f"start_time={start_time}&end_time={end_time}"
+    url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}&{}&{}".format(
+        query, tweet_fields, user_fields, filters
     )
     return url
 
